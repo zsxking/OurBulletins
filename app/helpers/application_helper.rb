9bd426d666ac9f214342a00ef3a0ac2e53a5ed2e
Shaoxuan Zhang <zsxking@gmail.com>
2011-04-15 08:38:09 -1000

    * Used helper to render the title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Return a title on a per-page basis.
+  def title
+    base_title = "Project Lounge"
+    if @title.nil?
+      base_title
+    else
+      "#{@title} | #{base_title}"
+    end
+  end
 end
