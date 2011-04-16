76b178620513e2c392537914bac76271e9aca036
Shaoxuan Zhang <zsxking@gmail.com>
2011-04-15 15:54:11 -1000

    * Renamed username to name for User model.   * Added initialize and formatted_email methods.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(:version => 20110414085700) do
 
   create_table "users", :force => true do |t|
-    t.string   "username"
+    t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
