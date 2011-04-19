384d1bc4e2e3929c1df17c7475a028823a4c0e8e
Shaoxuan Zhang <zsxking@gmail.com>
2011-04-18 22:35:37 -1000

    * Finished encrypted password and authentication methods in User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20110414085700
+# Schema version: 20110418100102
 #
 # Table name: users
 #
@@ -11,8 +11,9 @@
 #
 
 class User < ActiveRecord::Base
+  attr_accessor :password
   # Only these attributes have getter and setter for outside access.
-  attr_accessible :name, :email
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w]+[\w+\-.]*@[\w]+[.\-[\w]+]*\.edu\z/i
 
@@ -20,5 +21,46 @@ class User < ActiveRecord::Base
   validates :email, :presence   => true,
                     :format     => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 8..40 }
 
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    self.encrypted_password == encrypt(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+    #automatically returns nil at the end of the method.
+  end
+
+  #use a before_save callback to create encrypted_password just before the user is saved.
+  #we register a callback called encrypt_password by passing a symbol of that
+  #name to the before_save method, and then define an encrypt_password method
+  #to perform the encryption. With the before_save in place, Active Record will
+  #automatically call the corresponding method before saving the record.
+  before_save :encrypt_password
+
+  # all methods defined after private are private methods
+  private
+
+    def encrypt_password
+      self.salt = make_salt if self.new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+
+    def encrypt(string)
+      secure_hash("#{self.salt}::#{string}")
+    end
+
+    def make_salt
+      secure_hash("#{Time.now.utc}::#{self.password}")
+    end
+
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110418100102) do
+ActiveRecord::Schema.define(:version => 20110419020858) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
+    t.string   "encrypted_password"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
