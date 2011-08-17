13b4adf8dc6ccf72cc06283d733e8c231d2cffb6
Shaoxuan Zhang <zsxking@gmail.com>
2011-08-17 11:26:58 -1000

    Removed SearchesController.

--- app/controllers/searches_controller.rb
+++
@@ -1,9 +0,0 @@
-class SearchesController < ApplicationController
-  def index
-    @title = "Search"
-    @keywords = params[:q].to_s.split(' ')
-    @model = params[:cat] ? params[:cat].classify.constantize : Book
-    @results = @model.search_tank(params[:q])
-  end
-
-end
